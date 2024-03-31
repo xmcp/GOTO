9c52133015b9e0a87b46940c4b5e659e7f491798
xmcp <xmcp@pku.edu.cn>
2024-03-31 14:13:32 +0800

    update for mv3

--- background.js
+++ background.js
@@ -1,67 +1,83 @@
-localStorage['rules']=localStorage['rules']||JSON.stringify([
-  ['bilibili','^av(\\d+)$','http://bilibili.com/video/av$1'],
-  ['lydsy','^bz(\\d+)$','http://www.lydsy.com/JudgeOnline/problem.php?id=$1']
-]);
-localStorage['newtab']=localStorage['newtab']||'0';
+let rules = [];
+let newtab = false;
 
-rules=JSON.parse(localStorage['rules']);
+function load_config() {
+    chrome.storage.sync.get(['rules', 'newtab'], function(r) {
+        console.log('load_config', r);
+        rules = r['rules'] || [];
+        newtab = r['newtab'] || false;
+    });
+}
+load_config();
+chrome.storage.onChanged.addListener(load_config);
+
+chrome.runtime.onInstalled.addListener(function(details) {
+    if (details.reason==='install') {
+        chrome.storage.sync.set({
+            rules: [
+                ['bilibili','^av(\\d+)$','http://bilibili.com/video/av$1'],
+            ],
+            newtab: false,
+        });
+    }
+});
 
 function esc(unsafe) {
-  return unsafe.replace(/[<>&'"]/g, function(c) {
-    switch (c) {
-      case '<': return '&lt;';
-      case '>': return '&gt;';
-      case '&': return '&amp;';
-      case '\'': return '&apos;';
-      case '"': return '&quot;';
-    }
-  });
+    return unsafe.replace(/[<>&'"]/g, function(c) {
+        switch (c) {
+            case '<': return '&lt;';
+            case '>': return '&gt;';
+            case '&': return '&amp;';
+            case '\'': return '&apos;';
+            case '"': return '&quot;';
+        }
+    });
 }
 
 function open_tab(url) {
-  if(localStorage['newtab']==='1')
-    chrome.tabs.create({
-      url: url,
-      active: true,
-    });
-  else
-    chrome.tabs.query(
-      {active: true, currentWindow: true},
-      function(tabs) {
-        chrome.tabs.update(tabs[0].id, {url: url});
-      }
-    );
+    if(newtab)
+        chrome.tabs.create({
+            url: url,
+            active: true,
+        });
+    else
+        chrome.tabs.query(
+            {active: true, currentWindow: true},
+            function(tabs) {
+                chrome.tabs.update(tabs[0].id, {url: url});
+            }
+        );
 }
 
-chrome.omnibox.onInputChanged.addListener(function(text,suggest){
-  var sug=[];
-  rules.forEach(function(rule) {
-    var re=RegExp(rule[1]);
-    if(re.test(text))
-      sug.push({
-        content: '>>> '+text.replace(re,rule[2]),
-        description: '<dim>'+esc(text)+'</dim> <match>'+esc('<'+rule[0]+'>')+'</match> <url>'+esc(text.replace(re,rule[2]))+'</url>'
-      });
-  });
-  if(sug.length) {
-    chrome.omnibox.setDefaultSuggestion({description:sug.splice(0,1)[0].description});
-    suggest(sug);
-  } else {
-    chrome.omnibox.setDefaultSuggestion({description:'<dim>'+esc(text)+'</dim> &lt;无匹配&gt;'});
-  }
+chrome.omnibox.onInputChanged.addListener(function(text, suggest){
+    let sug = [];
+    rules.forEach(function([name, reg, rep]) {
+        let re = RegExp(reg);
+        if(re.test(text))
+            sug.push({
+                content: '>>> '+text.replace(re, rep),
+                description: '<dim>'+esc(text)+'</dim> <match>'+esc('<'+name+'>')+'</match> <url>'+esc(text.replace(re, rep))+'</url>'
+            });
+    });
+    if(sug.length) {
+        chrome.omnibox.setDefaultSuggestion({description:sug.splice(0, 1)[0].description});
+        suggest(sug);
+    } else {
+        chrome.omnibox.setDefaultSuggestion({description:'<dim>'+esc(text)+'</dim> &lt;无匹配&gt;'});
+    }
 });
 
 chrome.omnibox.onInputEntered.addListener(function(text){
-  if(text.indexOf('>>> ')==0)
-    open_tab(text.substr(4,text.length));
-  else {
-    for(var i=0;i<rules.length;i++) {
-      var re=RegExp(rules[i][1]);
-      if(re.test(text)) {
-        open_tab(text.replace(re,rules[i][2]));
-        return;
-      }
+    if(text.indexOf('>>> ')==0)
+        open_tab(text.substr(4, text.length));
+    else {
+        for(let [name, reg, rep] of rules) {
+            let re = RegExp(reg);
+            if(re.test(text)) {
+                open_tab(text.replace(re, rep));
+                return;
+            }
+        }
+        //alert('输入未匹配任何规则');
     }
-    alert('输入未匹配任何规则');
-  }
 });
--- options.js
+++ options.js
@@ -1,14 +1,12 @@
 function $$(x) {return document.getElementById(x);};
-$newtab=$$('newtab');
-$result=$$('result-body');
-
-rules=JSON.parse(localStorage['rules']);
+$newtab = $$('newtab');
+$result = $$('result-body');
 
 function esc(s) {
   return s.replace(/&/g,"&amp;").replace(/</g,"&lt;").replace(/>/g,"&gt;");
 }
 function esc_attr(s) { // http://stackoverflow.com/questions/7753448/how-do-i-escape-quotes-in-html-attribute-values
-    preserveCR='&#13;';
+    preserveCR = '&#13;';
     return ('' + s)
         .replace(/&/g, '&amp;')
         .replace(/'/g, '&apos;')
@@ -20,63 +18,65 @@ function esc_attr(s) { // http://stackoverflow.com/questions/7753448/how-do-i-es
         ;
 }
 
-function mk_editable(i) {
-  $$('result-'+i).innerHTML=
-    '<td><input id="box-r0-'+i+'" value="'+esc(rules[i][0])+'"></td>'+
-    '<td><input id="box-r1-'+i+'" value="'+esc(rules[i][1])+'"></td>'+
-    '<td><input id="box-r2-'+i+'" value="'+esc(rules[i][2])+'"></td>'+
-    '<td>'+
-      '<button type="button" id="save-btn-'+i+'">Save</button>'+
-      '<button type="button" id="del-btn-'+i+'">Delete</button>'+
-    '</td>';
-    $$('save-btn-'+i).addEventListener('click',function(){
-      rules[i][0]=$$('box-r0-'+i).value;
-      rules[i][1]=$$('box-r1-'+i).value;
-      rules[i][2]=$$('box-r2-'+i).value;
+chrome.storage.sync.get(['rules', 'newtab'], function({rules, newtab}) {
+    function mk_editable(i) {
+        $$('result-'+i).innerHTML=
+            '<td><input id="box-r0-'+i+'" value="'+esc(rules[i][0])+'"></td>'+
+            '<td><input id="box-r1-'+i+'" value="'+esc(rules[i][1])+'"></td>'+
+            '<td><input id="box-r2-'+i+'" value="'+esc(rules[i][2])+'"></td>'+
+            '<td>'+
+                '<button type="button" id="save-btn-'+i+'">Save</button>'+
+                '<button type="button" id="del-btn-'+i+'">Delete</button>'+
+            '</td>';
+        $$('save-btn-'+i).addEventListener('click',function(){
+            rules[i][0] = $$('box-r0-'+i).value;
+            rules[i][1] = $$('box-r1-'+i).value;
+            rules[i][2] = $$('box-r2-'+i).value;
+            save();
+        });
+        $$('del-btn-'+i).addEventListener('click',function(){
+            rules.splice(i,1);
+            save();
+        });
+    }
+
+    function save() {
+        chrome.storage.sync.set({
+            rules: rules,
+            newtab: newtab,
+        });
+        location.reload();
+    }
+
+    for(let i = 0; i<rules.length; i++) {
+        var row = document.createElement('tr');
+        row.id = 'result-'+i;
+        row.innerHTML=
+            '<td>'+esc(rules[i][0])+'</td>'+
+            '<td><pre>'+esc(rules[i][1])+'</pre></td>'+
+            '<td><pre>'+esc(rules[i][2])+'</pre></td>'+
+            '<td>'+
+                '<button type="button" id="edit-btn-'+i+'">Edit</button>'+
+                '<button type="button" id="del-btn-'+i+'">Delete</button>'+
+            '</td>';
+        
+        $result.appendChild(row);
+        $$('edit-btn-'+i).addEventListener('click',function(){
+            mk_editable(i);
+        });
+        $$('del-btn-'+i).addEventListener('click',function(){
+            rules.splice(i, 1);
+            save();
+        });
+    };
+
+    $newtab.checked = newtab;
+    $newtab.addEventListener('change',function(){
+      newtab = $newtab.checked;
       save();
     });
-    $$('del-btn-'+i).addEventListener('click',function(){
-      rules.splice(i,1);
+    $$('create').addEventListener('click',function(){
+      rules.push(['name','^$','http://$1']);
       save();
     });
-}
-
-function save() {
-  localStorage['rules']=JSON.stringify(rules);
-  chrome.extension.getBackgroundPage().rules=rules;
-  location.reload();
-}
-
-for(var i=0;i<rules.length;i++) {
-  var row=document.createElement('tr');
-  row.id='result-'+i;
-  row.innerHTML=
-    '<td>'+esc(rules[i][0])+'</td>'+
-    '<td><pre>'+esc(rules[i][1])+'</pre></td>'+
-    '<td><pre>'+esc(rules[i][2])+'</pre></td>'+
-    '<td>'+
-      '<button type="button" id="edit-btn-'+i+'">Edit</button>'+
-      '<button type="button" id="del-btn-'+i+'">Delete</button>'+
-    '</td>';
-  
-  $result.appendChild(row);
-  (function(i){
-    $$('edit-btn-'+i).addEventListener('click',function(){
-      mk_editable(i);
-    });
-    $$('del-btn-'+i).addEventListener('click',function(){
-      rules.splice(i,1);
-      save();
-    });
-  })(i);
-};
-
-$newtab.checked=localStorage['newtab']==='1';
-$newtab.addEventListener('change',function(){
-  localStorage['newtab']=$newtab.checked?'1':'0';
-  location.reload();
-});
-$$('create').addEventListener('click',function(){
-  rules.push(['name','^$','http://$1']);
-  save();
 });
